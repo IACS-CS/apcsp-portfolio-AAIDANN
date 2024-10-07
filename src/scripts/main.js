10da1b98814c7286a3ab0789b45d7186d1c19f69
AAIDANN <114506075+AAIDANN@users.noreply.github.com>
2024-10-07 16:14:59 +0000

    added first AOS effect

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,14 @@
 import "../styles/styles.css";
 import "@picocss/pico/css/pico.min.css";
 // Add imports here
-
+import Aos from "aos";
+import "aos/dist/aos.css";
 console.log("What?");
-console.log("Hello world, from main.js!");
 if (typeof window !== "undefined") {
   // Put code that runs ONLY in the browser here -- this is most likely where
+  Aos.init();
   // MOST of your code should go.
+  console.log("Hello World, from main.js");
   //
   // If you see a SSR error, try moving your code here!
   console.log("Hello world, browser :-)");
