7bed98356f985c0b7aa28b1afa6bf0726d9c97e3
AAIDANN <114506075+AAIDANN@users.noreply.github.com>
2024-10-17 23:45:30 +0000

    testing library

--- src/scripts/browser.js
+++ src/scripts/browser.js
@@ -1,18 +1,1 @@
-import Particles  from "particles.js";
-console.log('Imported',Particles)
-var particles = Particles.init({
-  selector: '.background',
-  color: ['#DA0463', '#404B69','pink','orange','grey','#c6093b','#0033a0'],
-  connectParticles: false,
-  sizeVariation: 1000,
-  maxParticles : 100,
-  responsive: [{
-    breakpoint: 800,
-    options: {
-      color: '#00C9B1',
-      maxParticles: 80,
-      connectParticles: false
-    }
-  }]
-});
-console.log('Running?',particles)
+import 'animate.css';
